95031015121352b5d2bb20f8d2dc71a8236bd86d
irina <irisha.karpova@gmail.com>
2021-01-18 15:02:54 -0600

    update

--- src/index.js
+++ src/index.js
@@ -2,23 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from '@material-ui/core/styles';
-import theme from './theme'
-import { images } from './images';
 
 ReactDOM.render(
-    <ThemeProvider theme={theme}>
-      <React.StrictMode>
-          <App
-              serverData={images}
-              containerMaxWidth={800}
-              containerRatio={1.5}
-              containerShadow={false}
-              imageShadow={false}
-              showNextPrev={false}
-           />
-      </React.StrictMode>
-    </ThemeProvider>,
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
